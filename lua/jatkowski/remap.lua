5479137d62dd3d10607b9666de28ee58a1e04940
Tomasz Jatkowski <tomasz.jatkowski@u2i.com>
2025-02-10 14:56:52 +0100

    New shortcuts

--- lua/jatkowski/remap.lua
+++ lua/jatkowski/remap.lua
@@ -8,4 +8,7 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent and reselect" })
 vim.keymap.set("v", "<", "<gv", { desc = "Outdent and reselect" })
 vim.keymap.set("n", ">", ">>")
 vim.keymap.set("n", "<", "<<")
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
